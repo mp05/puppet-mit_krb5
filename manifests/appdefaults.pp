29cc06acc3161b3e550064974255d00e8ce4dc31
mp05 <mikephil@gmail.com>
2017-02-06 17:28:34 +0000

    Update appdefaults.pp
    Started with the parameters

--- manifests/appdefaults.pp
+++ manifests/appdefaults.pp
@@ -2,6 +2,31 @@
 #
 # Configure appdefaults section of krb5.conf
 #
+# === Parameters
+#
+# [*application*]
+#    Two forms possible either a hash  or a nested hash
+#  1)
+#    application = {
+#      option1 = value
+#      option2 = value
+#    }
+#  2)
+#   application = {
+#      realm1 = {
+#        option = value
+#      }
+#     realm2 = {
+#        option = value
+#     }
+#   }
+# 
+# [*realm*]
+#  option / value pairs in hash
+#
+# [*option*]
+#  option / value pairs
+#
 # === Authors
 #
 # Patrick Mooney <patrick.f.mooney@gmail.com>
